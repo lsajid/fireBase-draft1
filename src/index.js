de772fd947b615139c1bdbfbce5af0f7288f58cc
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-02-19 14:42:12 -0500

    succesfully created firebase, localhost5000 is working

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth"
+
+
+//firebase App instance this instance is how firebaseSDK knows how to connect to specific firebase backend
+const firebaseApp = initializeApp({
+
+});
